be44c3bc770df11899d3f049074b147858be365f
bhoeijmakers <bart.jan.hoeijmakers@mavention.nl>
2015-12-03 12:01:42 +0100

    sdsdsd

---
+++ SharePointProject2/SharePointProject2/WebPart2/WebPart2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace SharePointProject2.WebPart2
+{
+    [ToolboxItemAttribute(false)]
+    public class WebPart2 : WebPart
+    {
+        protected override void CreateChildControls()
+        {
+        }
+    }
+}
